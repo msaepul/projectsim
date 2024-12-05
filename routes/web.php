78d1db56d1de42cda7bace03a127087befa0c247
ridhwan <erlanggaridhwan@gmail.com>
2024-12-05 16:00:39 +0700

    model surat dan list surat

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/surat/list', [FormController::class, 'list'])->name('form.list');
+    
 });
 
 require __DIR__.'/auth.php';
