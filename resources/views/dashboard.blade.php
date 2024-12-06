67cccdc430a3fbea1545041032607377b9ddb3c9
indahindrian <indrianiindah901@gmail.com>
2024-12-06 14:15:20 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,69 @@
 @extends('layouts.main')
 @section('content')
-    <!-- Main content -->
-    <h1>ini halaman dashboard</h1>
+
+    <h1>Selamat Datang, {{ $name }}</h1>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="card">
+                    <div class="card-header bg-blue">
+                        <h3 class="card-title">Surat Masuk</h3>
+                        <div class="card-tools">
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-text">100 Surat Masuk</h5>
+                        <a href="#" class="btn btn-light">Lihat Detail</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+                <div class="card">
+                    <div class="card-header bg-info">
+                        <h3 class="card-title">Surat Keluar</h3>
+                        <div class="card-tools">
+                            <i class="fas fa-paper-plane"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-text">50 Surat Keluar</h5>
+                        <a href="#" class="btn btn-light">Lihat Detail</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+                <div class="card">
+                    <div class="card-header bg-red">
+                        <h3 class="card-title">Surat Pending</h3>
+                        <div class="card-tools">
+                            <i class="fas fa-clipboard"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-text">20 surat</h5>
+                        <a href="#" class="btn btn-light">Lihat Detail</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+                <div class="card">
+                    <div class="card-header bg-green">
+                        <h3 class="card-title">Arsip</h3>
+                        <div class="card-tools">
+                            <i class="fas fa-archive"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-text">1000 Arsip</h5>
+                        <a href="#" class="btn btn-light">Lihat Detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
